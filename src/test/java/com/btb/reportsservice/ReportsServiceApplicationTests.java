3a86d0b9e621122b46e0bbf5ea0a0b74a60ec659
Edgar Doutón <38222402+lEdgaar@users.noreply.github.com>
2023-12-18 23:43:12 +0100

    Spring initializr

---
+++ src/test/java/com/btb/reportsservice/ReportsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btb.reportsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
